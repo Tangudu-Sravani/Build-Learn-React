46fe896a4ceb107dab0cfeb8f79de48317dd6ac1
unknown <sravanit2002@gmail.com>
2025-07-28 22:23:03 +0530

    Notes for Inception

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
+
+/* createRoot lets you create a root to display React components inside a browser DOM node.
+ A DOM element. React will create a root for this DOM element and allow you to call functions on the root, such as render to display rendered React content. */
+ // [browser DOM node defined in your HTML.]
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+
+//  the syntax of creating a react element [simple example]
 // const heading = React.createElement(  "h1",  {id:"heading" },  "Hello this is been printed by react"); 
 // reactRoot.render(heading);
 
 /*
-
+- want to generate this structure using react
 <div id="parent">
     <div id="child">
         <h1>Im a heading</h1>
     </div>
 </div>
-
-
 */
+
+// code for recreting the above structure using react
+
+// can be Noticed compared to  JS and HTMl its a bit long in React
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
---
+++ README.md
@@ -0,0 +1,14 @@
+-------------------------------------------------
+Ways to use react
+1. CDN Links
+2. using import of libraries of react
+
+use of CDN links of react
+    - 2 CDN links 
+        i . is used for react
+        ii. specific react [react-dom use for web application , Native is used for mabile application etc.. ]
+
+ReactDOM.createRoot
+    ReactDOM contains a method called createRoot - which creates a root in react , renders React component in browser within the element provided to it
+
+
