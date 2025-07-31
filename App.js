d5771c267a48d597e082c4ab38d33ab6e69a8481
unknown <sravanit2002@gmail.com>
2025-07-31 23:46:15 +0530

    use of babel

--- App.js
+++ App.js
@@ -2,18 +2,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+// creating a element using React
+// React.CreateElement - React Element => JS object => HTMLElement (render)
 
-// creating a react div element  [JSX] - file name .js is valid one for it
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
- [ React.createElement("div",
-    { id: "child" },
-    [React.createElement("h1", {}, "This is the heading finally"),React.createElement("h1", {}, " im child 2 ")]
-  ),  React.createElement("div",
-    { id: "child" },
-    [React.createElement("h1", {}, "This is the heading finally"),React.createElement("h1", {}, " im child 2 ")]
-  )]
-);
+//React Element using core React
+const heading = React.createElement("h1",{id:'heading1'},"This is a short heading");
 
-reactRoot.render(parent) 
+//JSX :- JSX is not HTML in JS , [It is like HTML like syntax]
+//creating a h1 tag in JSX
+//Creating React lement using JSX
+const jsxHeading = <h1 id="heading">Hello JSX</h1>
+
+
+// as core react syntax is not too much not readable - so they created JSX
+//JSX syntax  is not pure valid JS ,JS engine can understand JSX code
+//react.render also cant understands the JSX code 
+//  - here comes the role of PARCEL which contains BABEL  makes our job easy
+// JSX (transpiled before it reaches the JS Engine)
+//  JSX [through babel]=> React.createElement => JS object => HTML render in browser
+const jsxHeading1 = <h1 id="heading">Hello JSX</h1>
+
+console.log("heading ",heading);
+reactRoot.render(heading) 
