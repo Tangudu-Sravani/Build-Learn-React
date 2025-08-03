1d63758dccc4e0f854c879fc61136c8dfcddf75b
unknown <sravanit2002@gmail.com>
2025-08-03 15:34:45 +0530

    Use of JS in JSX

--- App.js
+++ App.js
@@ -2,35 +2,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-// const heading = React.createElement("h1",{id:'heading1'},"This is a  heading");
-
-// console.log("heading ",heading);
+//React Element
 const jsxHeading = <h1 id="heading">Hello JSX</h1>
-
-//react Component 
-// Functional Component - OLD
-// Functional Component - NEW 
-
-
-// focusing on Functional Component
-// Functional Component :- its just a normal JS Function which returns some JSX React Component Should be name start with CAPITAL letter
-
-const HeadingComponent = () =><h1>Heading Function Component</h1>
-
-
-// Component Composition :- reusable component , component inside component 
-const Title = () => {
-  return (
+// JS inside JSX
+const Title = () => (
     <div className="title">
       <h1>Title Function Component</h1>
+      {jsxHeading}
       <HeadingComponent />
+      
+      {/* another way to include component 
+       <HeadingComponent></HeadingComponent>
+       {HeadingComponent()}
+       */}
+       
     </div>
-  );
-};
+);
 
 
+const number = 1234;
+const HeadingComponent = () =>(
+<div id="container">
+     {number}
+     <h1 className="heading">Heading React Functional Component</h1>
+
+</div>
+)
 
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
-//how babel understands that is a jsx component , there are angular brackets , so babel transpiles before rendering
+
 reactRoot.render(<Title />) 
