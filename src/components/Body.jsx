8d849522eb2284fce48a017b0c025527044e7bc7
unknown <sravanit2002@gmail.com>
2025-08-06 00:20:34 +0530

    OverComing the limitation use of JS Varible by useState Hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,11 +7,17 @@ import resList from '../utils/mockData';
 
 const Body = () => {
  
-  //Normal JS variable
-  let listOfRestaurants = [
+/* local State Variable - super  powerful variable
+  react element is a plain JS object 
+  similarly a hook is normal JS function [written by facebook devplrs] given use by react by utilites, a function where logic written behind [where we got utilites in npm install they will be downloaded ]
+  hook's - are utility functions */
+
+  // useState returns an array - so have to wrap received variable with [][square brackets]
+
+  const [ listOfRestaurants, setlListOfRestaurants ] = useState([ 
     {
       data: {
-        id: "121603",
+        id: "121601",
         name: "Dominos",
         cuisines: ["Kerala", "Chinese"],
         costForTwo: 30000,
@@ -20,20 +26,30 @@ const Body = () => {
         avgRating: "3.9",
       },
     },
-     {
+      {
       data: {
-        id: "121604",
-        name: "KFC",
-        cuisines: ["Kerala", "Chinese"],
+        id: "121602",
+        name: "Biryani by kilo",
+        cuisines: ["Andra", "Chenayee"],
         costForTwo: 30000,
         deliveryTime: 24,
         restaurantId: "121603",
-        avgRating: "5.0",
+        avgRating: "2.9",
       },
     },
-  ];
-  // in case if we use normal JS variable then the data of filtered result the enginee will be getting but because there will be no rendering so in UX we will not be see the changes
-  //hence dependent on useState Hook
+      {
+      data: {
+        id: "121603",
+        name: "KFC",
+        cuisines: ["Haryana", "punjab"],
+        costForTwo: 30000,
+        deliveryTime: 24,
+        restaurantId: "121603",
+        avgRating: "4.9",
+      },
+    }
+  
+  ]);// whatever  useStae() is containing here at defination is default value
 
   return (
     <div className="body">
@@ -41,7 +57,7 @@ const Body = () => {
         <button className="filter-btn" onClick={()=>{
           //Filter logic
           let filteredList = listOfRestaurants.filter((res)=>res.data.avgRating>4);
-          listOfRestaurants = filteredList; // this will not re-render the component
+          setlListOfRestaurants(filteredList);// this will does the re-render the component, if use-state varible gets updated
            {console.log("listOfRestaurants", listOfRestaurants)}
         }}>Top Rated restaurant</button>
        
