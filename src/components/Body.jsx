a5f9b13a8e3b3952c73a46bb6202c873770b8068
unknown <sravanit2002@gmail.com>
2025-08-06 00:35:08 +0530

    05 Finished Let's get Hooked

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,51 +6,7 @@ import resList from '../utils/mockData';
 
 
 const Body = () => {
- 
-/* local State Variable - super  powerful variable
-  react element is a plain JS object 
-  similarly a hook is normal JS function [written by facebook devplrs] given use by react by utilites, a function where logic written behind [where we got utilites in npm install they will be downloaded ]
-  hook's - are utility functions */
-
-  // useState returns an array - so have to wrap received variable with [][square brackets]
-
-  const [ listOfRestaurants, setlListOfRestaurants ] = useState([ 
-    {
-      data: {
-        id: "121601",
-        name: "Dominos",
-        cuisines: ["Kerala", "Chinese"],
-        costForTwo: 30000,
-        deliveryTime: 24,
-        restaurantId: "121603",
-        avgRating: "3.9",
-      },
-    },
-      {
-      data: {
-        id: "121602",
-        name: "Biryani by kilo",
-        cuisines: ["Andra", "Chenayee"],
-        costForTwo: 30000,
-        deliveryTime: 24,
-        restaurantId: "121603",
-        avgRating: "2.9",
-      },
-    },
-      {
-      data: {
-        id: "121603",
-        name: "KFC",
-        cuisines: ["Haryana", "punjab"],
-        costForTwo: 30000,
-        deliveryTime: 24,
-        restaurantId: "121603",
-        avgRating: "4.9",
-      },
-    }
-  
-  ]);// whatever  useStae() is containing here at defination is default value
-
+ const [ listOfRestaurants, setlListOfRestaurants ] = useState(resList);
   return (
     <div className="body">
       <div className="search">
