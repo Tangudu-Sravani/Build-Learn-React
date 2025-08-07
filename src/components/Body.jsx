c67f9230d031bec5de698c8653b0e73a723a528a
unknown <sravanit2002@gmail.com>
2025-08-07 22:35:12 +0530

    06 Exploring the world - UseEffect importance and usage

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RestaurantCard from './RestaurantCard';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import resList from '../utils/mockData';
 
 
 const Body = () => {
  const [ listOfRestaurants, setlListOfRestaurants ] = useState(resList);
+ //hook is nothing but normal JS function which react gives to us, it has its own specific function
+ //useEffect hook it contains two parameters callbackfunctiona and depedndecy array [dependency array is it is optional]
+ // this is called after component is rendered
+ useEffect(()=>{
+  console.log("useEffect called");
+ },[]);
+ // first body rendered is called then useEffect will be called
+ cosole.log("Body rendered");
   return (
     <div className="body">
       <div className="search">
         <button className="filter-btn" onClick={()=>{
-          //Filter logic
           let filteredList = listOfRestaurants.filter((res)=>res.data.avgRating>4);
-          setlListOfRestaurants(filteredList);// this will does the re-render the component, if use-state varible gets updated
+          setlListOfRestaurants(filteredList);
            {console.log("listOfRestaurants", listOfRestaurants)}
         }}>Top Rated restaurant</button>
        
