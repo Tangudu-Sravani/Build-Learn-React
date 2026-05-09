ab0b66239575eb7c930a7192436fddd0c5023e0a
Tangudu Sravani <sravanit2002@gmail.com>
2026-05-10 00:01:10 +0530

    Basic ContactUs test [JEST]

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -2,11 +2,28 @@ import { render, screen } from "@testing-library/react";
 import Contact from '../Contact'; // adjust if needed
 import "@testing-library/jest-dom"; 
 
-test("Should load contact page", () => {
-    render(<Contact />); // for this babel config have beeen added and  npm install @babel/preset-react is done
-    // const heading = screen.getByRole("heading");// trying to render component onto JSDOM and trying to find heading inside 
 
-     const text = screen.getByText("Hii Random heading");
-     const placeholderText = screen.getByPlaceholderText("Enter your name");
-    expect(placeholderText).toBeInTheDocument(); //trying to find wheather heading is present or not - for this "@testing-library/jest-dom" is required 
+test("Should load contact us componenet",() => {
+    render (<Contact />);
+
+
+    const heading = screen.getByRole("heading", {
+        name: "Contact us page",
+    });// for this babel config have beeen added and  npm install @babel/preset-react is done
+    // trying to render component onto JSDOM and trying to find heading inside 
+    expect(heading).toBeInTheDocument();
+     //trying to find wheather heading is present or not - for this "@testing-library/jest-dom" is required 
+})
+
+test("check for button in contact page", () => {
+    render(<Contact />);
+
+    const text = screen.getByText("Hii Random heading");
+
+    const placeholderText =
+        screen.getByPlaceholderText("Enter your name");
+        //find image - getAltText 
+
+    expect(text).toBeInTheDocument();
+    expect(placeholderText).toBeInTheDocument();
 });
